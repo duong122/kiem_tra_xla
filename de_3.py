be9b3e8b49c026f093d518c97ea7a8b3466a81ad
duong122 <131834049+duong122@users.noreply.github.com>
2024-05-01 01:47:25 +0700

    j

--- de_3.py
+++ de_3.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-I = cv.imread(r"D:\Image_processing\color_split.png")
+I = cv.imread(r"D:\Image_processing\color_split.png", cv.IMREAD_COLOR)
 cv.imshow("origin image", I)
 Ihsv = cv.cvtColor(I, cv.COLOR_BGR2HSV)
 h, s, v = cv.split(Ihsv)
@@ -11,8 +11,14 @@ max_v = v.max()
 print("Muc sang lon nhat cua kenh mau v la: ", max_v)
 
 kernel = np.ones((5, 5), np.float32) / 25
-img = cv.filter2D(I, -1, kernel)
-cv.imshow("anh sau khi loc trung binh la ", img)
+Is = cv.filter2D(I, -1, kernel)
+cv.imshow("anh sau khi loc trung binh la ", Is)
+print(Is)
+
+Is_2 = 255 - Is
+_, Ib = cv.threshold(Is_2, 0, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+cv.imshow("Ib", Ib)
+
 
 
 
